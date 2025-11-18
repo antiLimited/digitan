cf7d75073249fbdd605fa28f666151c70bf02a83
antiLimited <webmaster@antilimit.dev>
2025-11-17 20:40:33 -0500

    init MAYBE

--- main.ts
+++ main.ts
@@ -1,2 +1,13 @@
-import { Client, GatewayIntentBits } from "discord.js"
+import { Client, Events, GatewayIntentBits } from "discord.js"
 import * as dotenv from "dotenv"
+dotenv.config()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
